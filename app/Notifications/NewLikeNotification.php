638c6693179bc8adfdda30be66563a711fdb2e06
GianfrancoLarocca <gianfrancolarocca2797@gmail.com>
2025-04-12 12:02:47 +0200

    feat(notifications): notify video owner when a like is received

--- app/Notifications/NewLikeNotification.php
+++ app/Notifications/NewLikeNotification.php
@@ -5,27 +5,30 @@ namespace App\Notifications;
 use App\Models\Video;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
-use Illuminate\Notifications\Messages\DatabaseMessage;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class NewLikeNotification extends Notification
+class NewLikeNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    public function __construct(protected Video $video) {}
+    public $video;
+
+    public function __construct(Video $video)
+    {
+        $this->video = $video;
+    }
 
     public function via(object $notifiable): array
     {
         return ['database'];
     }
 
-    public function toDatabase(object $notifiable): DatabaseMessage
+    public function toDatabase(object $notifiable): array
     {
-        return new DatabaseMessage([
-            'type' => 'like',
+        return [
+            'message' => 'Il tuo video "' . $this->video->title . '" ha ricevuto un nuovo like.',
             'video_id' => $this->video->id,
-            'video_title' => $this->video->title,
             'video_slug' => $this->video->slug,
-            'liked_by' => auth()->user()?->name,
-        ]);
+        ];
     }
 }

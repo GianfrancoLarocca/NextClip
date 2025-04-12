76dfb8f808dbe8129dc761f61f998aabab066811
GianfrancoLarocca <gianfrancolarocca2797@gmail.com>
2025-04-12 22:04:06 +0200

    feat: add VideoHistory model with relationships to user and video

--- app/Models/VideoHistory.php
+++ app/Models/VideoHistory.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class VideoHistory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'video_id',
+    ];
+
+    public $timestamps = true; // serve per registrare la data della visualizzazione
+
+    /**
+     * Utente che ha visto il video
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Video visto
+     */
+    public function video()
+    {
+        return $this->belongsTo(Video::class);
+    }
 }
